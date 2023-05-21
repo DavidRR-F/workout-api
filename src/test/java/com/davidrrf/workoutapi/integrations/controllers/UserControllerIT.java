c2a387aed2b9eef33d1acff26fb2c02ba0568eb5
DavidRR-F <david.rosefranklin96@gmail.com>
2023-05-21 09:07:42 -0400

    IT update user controller

--- src/test/java/com/davidrrf/workoutapi/integrations/controllers/UserControllerIT.java
+++ src/test/java/com/davidrrf/workoutapi/integrations/controllers/UserControllerIT.java
@@ -1,5 +1,6 @@
 package com.davidrrf.workoutapi.integrations.controllers;
 
+import com.davidrrf.workoutapi.dtos.UserUpdateRequest;
 import com.davidrrf.workoutapi.entities.User;
 import com.davidrrf.workoutapi.integrations.AbstractContainerBaseTest;
 import com.davidrrf.workoutapi.repositories.UserRepository;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -23,17 +25,20 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.AUTO_CONFIGURED)
 public class UserControllerIT extends AbstractContainerBaseTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private UserService userService;
     @MockBean
     private UserRepository userRepository;
     @Autowired
@@ -55,7 +60,7 @@ public class UserControllerIT extends AbstractContainerBaseTest {
     public void givenUserObject_whenCreateUser_thenReturnUser() throws Exception {
         User user = User.builder().firstName("John").lastName("Doe").email("johnDoe@gmail.com").build();
         // given
-        given(userRepository.save(ArgumentMatchers.any(User.class)))
+        given(userRepository.findById(user.getId()))
                 .willAnswer((invocation) -> invocation.getArgument(0));
         // when
         ResultActions response =  mockMvc.perform(post("/users")
@@ -68,6 +73,52 @@ public class UserControllerIT extends AbstractContainerBaseTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.lastName", CoreMatchers.is(user.getLastName())))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.email", CoreMatchers.is(user.getEmail())))
         ;
+    }
+
+    @Test
+    public void givenUserId_whenGetUser_thenReturnUser() throws Exception {
+        User user = User.builder().firstName("John").lastName("Doe").email("johnDoe@gmail.com").build();
+        // given
+        given(userRepository.findById(user.getId())).willReturn(Optional.of(user));
+        // when
+        ResultActions response =  mockMvc.perform(get("/users/{userId}", user.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(user))
+        );
+        // then
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName", CoreMatchers.is(user.getFirstName())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName", CoreMatchers.is(user.getLastName())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email", CoreMatchers.is(user.getEmail())))
+        ;
+    }
+
+    @Test
+    public void givenUserIdAndUpdatedInfo_whenUpdateUser_thenReturnUpdatedUser() throws Exception {
+        int userId = 1;
+        UserUpdateRequest userUpdateRequest = new UserUpdateRequest();
+        userUpdateRequest.setFirstName("UpdatedFirstName");
+        userUpdateRequest.setLastName("UpdatedLastName");
+        userUpdateRequest.setEmail("updatedEmail@gmail.com");
+
+        User updatedUser = User.builder().id(userId).firstName(userUpdateRequest.getFirstName())
+                .lastName(userUpdateRequest.getLastName()).email(userUpdateRequest.getEmail()).build();
+
+        // given
+        given(userService.updateUser(userId, userUpdateRequest)).willReturn(updatedUser);
+
+        // when
+        ResultActions response = mockMvc.perform(put("/users/{userId}", userId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userUpdateRequest))
+        );
+
+        // then
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", CoreMatchers.is(userId)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName", CoreMatchers.is(userUpdateRequest.getFirstName())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName", CoreMatchers.is(userUpdateRequest.getLastName())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email", CoreMatchers.is(userUpdateRequest.getEmail())));
     }
 
 }
